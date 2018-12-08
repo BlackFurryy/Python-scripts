e74aca8722edb7d88410398fd062f5d7f3c3ad04
BlackFurryy <40524444+BlackFurryy@users.noreply.github.com>
2018-12-08 12:28:25 +0530

    bash shell welcome
    Add python script in .bashrc file and see messages on each come back.

---
+++ Bash prompt/bash.py
@@ -0,0 +1,51 @@
+#Not Related to Config from now
+color='\e[0;32m'
+
+NC='\e[0m'
+
+a=`date|cut -c 19`
+
+var=(" Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.
+\n \t\t\t\t\t\t\t-Samuel Beckett " "Never give up, for that is just the place and time that the tide will turn.
+\n \t\t\t\t\t\t\t-Harriet Beecher Stowe " "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.
+\n \t\t\t\t\t\t\t-Thomas A. Edison" "Life isn't about getting and having, it's about giving and being.
+\n \t\t\t\t\t\t\t-Kevin Kruse" "Strive not to be a success, but rather to be of value.
+\n \t\t\t\t\t\t\t-Albert Einstein" "May you do good and not evil. May you find forgiveness for yourself and forgive others.\nMay you share freely, never taking more than you give.
+\n \t\t\t\t\t\t\t-Lord Buddha" "You miss 100% of the shots you don't take.
+\n \t\t\t\t\t\t\t-Wayne Gretzky" "People who are unable to motivate themselves must be content with mediocrity, no matter how impressive their other talents.
+\n \t\t\t\t\t\t\t-Andrew Carnegie" "Design is not just what it looks like and feels like. Design is how it works.
+\n \t\t\t\t\t\t\t-Steve Jobs" "Only those who dare to fail greatly can ever achieve greatly.
+\n \t\t\t\t\t\t\t-Robert F. Kennedy" "All our dreams can come true, if we have the courage to pursue them.
+\n \t\t\t\t\t\t\t-Walt Disney " "Success consists of going from failure to failure without loss of enthusiasm.
+\n \t\t\t\t\t\t\t-Winston Churchill" )
+
+
+echo -e "${color}--------------------------------------------------------------------------------"
+echo "****************             Welcome back Mr. Singh!             *****************"
+
+#echo -e "\n${var[$a]}"
+#echo -e  "\n
+#                     _oo0oo_
+#                    o8888888o
+#                    88" . "88
+#                    (| -_- |)
+#                    0\  =  /0
+#                  ___/'----'\___
+#                ." \\|"     "|// ".
+#               / \\|||  :  |||// \
+#              / _||||| -:- |||||- \
+#             |   | \\\  -  /// |   |
+#             | \_|  ''\---/''  |_/ |
+#             \  .-\__  "_"  ___/-. /
+#           ___'. .'  /--.--\  `. .'___
+#        ."" '<  `.___\_<|>_/___.' >' "".
+#       | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#       \  \ `_.   \_ __\ /__ _/   .-` /  /
+#   =====`-.____`.___ \_____/___.-`___.-'=====
+#                     =---="
+#
+#echo -e  "\nMay you do good and not evil.\n
+#May you find forgiveness for yourself and forgive others.\n
+#May you share freely, never taking more than you give."
+
+echo -e "--------------------------------------------------------------------------------${NC}"
